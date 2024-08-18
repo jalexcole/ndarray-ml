8d286ae050cf8c5ad60dfd78bc88aca618468c02
Alex <jalexcole@gmail.com>
2024-08-18 12:10:49 -0500

    more changes

--- src/bandits.rs
+++ src/bandits.rs
@@ -1,26 +1,61 @@
 use core::fmt::{Debug, Display};
 
+use ndarray::{Array1, Array2};
+
 pub trait Bandit: Display + Debug {
     type Step;
     type NArms;
 
-    fn hyperparameter(&self);
-    fn oracle_payoff(&self);
-
-    fn pull(&self);
-
-    fn reset(&self);
-
-    fn __pull(&self);
+    fn hyperparameter(&self) -> Hyperparameters;
+    fn oracle_payoff(&self, context: Option<Array1<f64>>) -> (f64, usize);
+    fn pull(&mut self, arm_id: usize, context: Option<Array1<f64>>) -> f64;
+    fn reset(&mut self);
 }
 
-pub struct MultinomialBandit {}
+// Struct to hold the hyperparameters of the bandit
+#[derive(Debug, Clone, PartialEq)]
+pub struct Hyperparameters {
+    id: String,
+    payoffs: Option<Array2<f64>>,
+    payoff_probs: Option<Array2<f64>>,
+    K: Option<usize>,
+    D: Option<usize>,
+    payoff_variance: Option<Vec<f64>>,
+    thetas: Option<Array2<f64>>,
+}
+
+pub struct MultinomialBandit {
+    payoffs: Array2<f64>,
+    payoff_probs: Array2<f64>,
+    arm_evs: Vec<f64>,
+    best_ev: f64,
+    best_arm: usize,
+    step: usize,
+    n_arms: usize,
+    hyperparameters: Hyperparameters,
+}
 
 impl MultinomialBandit {}
 
-pub struct BernoulliBandit {}
+pub struct BernoulliBandit {
+    payoffs: Array2<f64>,
+    arm_evs: Vec<f64>,
+    best_ev: f64,
+    best_arm: usize,
+    step: usize,
+    n_arms: usize,
+    hyperparameters: Hyperparameters,
+}
 
-pub struct GaussianBandit {}
+pub struct GaussianBandit {
+    means: Vec<f64>,
+    variances: Vec<f64>,
+    best_ev: f64,
+    best_arm: usize,
+    step: usize,
+    n_arms: usize,
+    hyperparameters: Hyperparameters,
+}
 
 pub struct ShortestPathBandit {}
 
